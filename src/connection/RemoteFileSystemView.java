3661b431f820e2fa1d4071fa340c538aed38c5b8
Sam Macaluso <willsc8forwings@gmail.com>
2013-08-29 08:39:38 -0400

    More work towards new SSH system: RemoteFileSystemViewer will create a folder which will house the files downloaded for editing

--- src/connection/RemoteFileSystemView.java
+++ src/connection/RemoteFileSystemView.java
@@ -7,17 +7,32 @@ package connection;
 import javax.swing.filechooser.FileSystemView;
 import com.jcraft.jsch.*;
 import java.io.File;
+import java.util.Random;
 import java.io.IOException;
-
 /**
  *
  * @author Sam
  */
 public class RemoteFileSystemView extends FileSystemView{
     
+    // NOTE: I still need to figure out what variables i need/don't need
+    private static final char[] symbols = new char[36];
+    private char[] buff;
+    private Random random = new Random();
+    
+    static { // We want symbols to have every alphanumeric value 0-9:a-z
+        for (int idx = 0; idx < 10; ++idx)
+            symbols[idx] = (char) ('0' + idx);
+        for (int idx = 10; idx < 36; ++idx)
+            symbols[idx] = (char) ('a' + idx - 10);
+  }
+    
     public static final String FILE_SEPERATOR = "/";
     private final Session sesConnection;
+    private File currLocalFile;
     private static ChannelSftp sftpChannel;
+    private String homeDirectory;
+    private String sessionName; // What we'll call the root folder in the tempdir
     
     public RemoteFileSystemView(Session sesConnection){
         this.sesConnection = sesConnection;
@@ -29,11 +44,22 @@ public class RemoteFileSystemView extends FileSystemView{
         } catch( JSchException ioX){
             System.out.println(ioX.getMessage());
         }
+        
+        sessionName = generateSessionName();
+        File localRoot = new File( System.getProperty("java.io.tmpdir") + sessionName );
+        if( !localRoot.mkdir() )
+            System.out.println("Error creating temporary ");
+        currLocalFile = localRoot;
+        
+        
     }
     
-    /*public RemoteFile downloadFile( RemoteFile toGet ){
-     * 
-     * }*/
+    private String generateSessionName(){
+        buff = new char[10];
+        for( int i = 0; i < buff.length;i++)
+            buff[i] = symbols[ random.nextInt( symbols.length ) ];
+        return new String( buff );
+    }
     
     
     // 
